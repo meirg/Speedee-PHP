67d4bbf92aac00cf5abddca0145c871ed0d88b30
Meir Goldstein <meirgoldstein06@gmail.com>
2015-04-07 17:00:50 -0500

    Added some error checking

--- Speedee.php
+++ Speedee.php
@@ -8,9 +8,11 @@ function GetFileContents($rel_name){
 
 function _GetZoneTable($origin_zip){
 	$csv = explode(PHP_EOL, GetFileContents('ZoneTables.CSV'));
+	if(count($csv) <= 2)	return false;
 	array_shift($csv); // Skip Headers
 	while(!is_null($r = array_shift($csv))) {
 		$r = str_getcsv($r);
+		if(count($r) < 4) continue;
 		if($origin_zip >= $r[1] && $origin_zip <= $r[2])
 			return $r[3];
 	}
@@ -19,9 +21,11 @@ function _GetZoneTable($origin_zip){
 
 function _GetZoneFromTable($table, $dest_zip){
 	$csv = explode(PHP_EOL, GetFileContents('zones/SP'.(int)substr($table, 2).'.CSV'));
+	if(count($csv) <= 2)	return false;
 	array_shift($csv); // Skip Headers
 	while(!is_null($r = array_shift($csv))) {
 		$r = str_getcsv($r);
+		if(count($r) < 4) continue;
 		if($dest_zip >= $r[1] && $dest_zip <= $r[2])
 			return $r[3];
 	}
@@ -30,10 +34,12 @@ function _GetZoneFromTable($table, $dest_zip){
 
 function _GetRate($zone, $weight){
 	$csv = explode(PHP_EOL, GetFileContents('RatesOnly.csv'));
+	if(count($csv) <= 2)	return false;
 	$weight = ceil($weight);
 	foreach($csv as $max_weight => $zones){
 		if($weight <= $max_weight + 1){
 			$zones = str_getcsv($zones);
+			if(count($zones) < $zone - 1) continue;
 			if(isset($zones[$zone - 2]))
 				return (float)$zones[$zone - 2];
 		}
